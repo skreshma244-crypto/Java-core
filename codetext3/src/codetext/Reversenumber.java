4d5a5d3acc8ff28ca51d8027af62bf6a7471d863
Reshma-Techprofile <skreshma244@gmail.com>
2025-09-14 11:30:55 +0530

    Reversenumber

--- codetext3/src/codetext/Reversenumber.java
+++ codetext3/src/codetext/Reversenumber.java
@@ -1,5 +1,16 @@
 package codetext;
 
 public class Reversenumber {
+	public static void main(String[]args) {
+		int a=3436;
+		int rev=0;
+		while(a>=0) {
+			int last=a%10;
+			rev=rev*10+last;
+			a=a%10;
+			
+		}
+		System.Out.Println(rev);
+	}
 
 }
